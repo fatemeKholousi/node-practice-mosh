7aef67349c0426712302a006437731a986274994
Fateme <fatemekholousizaer@gmail.com>
2022-02-07 21:38:26 +0330

    fix: joi package added

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
 
+const authors = [
+  { id: 1, name: "L.M.M" },
+  { id: 2, name: "R.L.Stine" },
+  { id: 3, name: "Jane webster" },
+];
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hi Baly");
+  res.send("Hi Baily");
 });
+
 app.get("/api/authors", (req, res) => {
   res.send([1, 2, 3]);
 });
-app.listen(3000, () => {
-  console.log("Listening");
+
+const port = process.env.PORT || 3000;
+// in terminal:
+// set PORT=6000
+app.listen(port, () => {
+  console.log(`listen you from ${port}`);
 });
 // app.post()
 // app.put()
 // app.delete()
+
+app.get("/api/authors/:id", (req, res) => {
+  const authy = authors.find((item) => item.id === parseInt(req.params.id));
+  if (!authy) res.status(404).send("404!");
+  res.send(authy);
+});
+
+app.post("/api/authors", (req, res) => {
+  const schema = { name: Joi.string().min(3) };
+  const newAuthor = { id: authors.length + 1, name: req.body.name };
+  authors.push(newAuthor);
+  res.send(newAuthor);
+});
