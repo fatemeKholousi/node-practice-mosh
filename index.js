0825bb079f7366c46e6b3571179f4776e7dbc670
Fateme <fatemekholousizaer@gmail.com>
2022-02-08 22:54:30 +0330

    fix: crud practice just finished

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const authors = [
   { id: 3, name: "Jane webster" },
 ];
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hi Baily");
 });
 
 app.get("/api/authors", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(authors);
 });
 
 const port = process.env.PORT || 3000;
@@ -34,4 +35,36 @@ app.post("/api/authors", (req, res) => {
   const newAuthor = { id: authors.length + 1, name: req.body.name };
   authors.push(newAuthor);
   res.send(newAuthor);
+});
+
+app.put("/api/authors/:id", (req, res) => {
+  const authy = authors.find((item) => item.id === parseInt(req.params.id));
+  if (!authy) res.status(404).send("404!");
+
+  // console.log(result);
+  // const result = validateAuthy(req.body);
+  const { error } = validateAuthy(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  authy.name = req.body.name;
+  res.send(authy);
+});
+
+function validateAuthy(authy) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(authy);
+}
+
+app.delete("/api/authors/:id", (req, res) => {
+  const authy = authors.find((item) => item.id === parseInt(req.params.id));
+  if (!authy) res.status(404).send("404!");
+  const index = authors.indexOf(authy);
+  authors.splice(index, 1);
+
+  res.send(authy);
 });
