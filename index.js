19884bb204e0990538e80ce21454e81cf37bef3e
Fateme <fatemekholousizaer@gmail.com>
2022-02-12 21:13:50 +0330

    promise

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("before");
-getUser(1212, displayUser);
+
+getUser(1)
+  .then((user) => getRepositories(user))
+  .then((repos) => getCommits(repos))
+  .then((commits) => console.log(commits))
+  .catch((err) => console.log(err));
 
 console.log("after");
 
@@ -15,21 +20,39 @@ function displayCommits(commits) {
   console.log(commits);
 }
 
-function getCommits(commits, callBack) {
-  setTimeout(() => {
-    return callBack({ id: 235, message: "fix:" });
-  }, 200);
+function getCommits(commits) {
+  console.log("commit=", commits);
+
+  return new Promise(
+    (resolve, reject) =>
+      setTimeout(() => resolve({ id: 235, message: "fix:" })),
+    2000
+  );
+
+  // setTimeout(() => {
+  //   return callBack({ id: 235, message: "fix:" });
+  // }, 200);
 }
 
-function getUser(id, callBack) {
-  setTimeout(() => {
-    return callBack({ id: id, gitHubUserName: "Fateme" });
-  }, 200);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    console.log("read from db...");
+    setTimeout(() => {
+      resolve({ id: id, gitHubUserName: "Fateme" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callBack) {
-  console.log("my git user name is : " + username);
-  setTimeout(() => {
-    return callBack(["rep1", "rep2", "rep3"]);
-  }, 2000);
+// setTimeout(() => {
+//   return callBack({ id: id, gitHubUserName: "Fateme" });
+// }, 200);
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    console.log(username);
+    setTimeout(() => {
+      return resolve(["rep1", "rep2", "rep3"]);
+    }, 2000);
+  });
+  // console.log("my git user name is : " + username);
 }
