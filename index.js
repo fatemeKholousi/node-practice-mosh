a32d289161fdf8faf510e45f428d8130d723aff3
Fateme <fatemekholousizaer@gmail.com>
2022-02-14 21:45:34 +0330

    async await -promise.all

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 console.log("before");
 
-getUser(1)
-  .then((user) => getRepositories(user))
-  .then((repos) => getCommits(repos))
-  .then((commits) => console.log(commits))
-  .catch((err) => console.log(err));
+// getUser(1)
+//   .then((user) => getRepositories(user))
+//   .then((repos) => getCommits(repos))
+//   .then((commits) => console.log(commits))
+//   .catch((err) => console.log(err));
 
-console.log("after");
+async function displayCommitsss() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUserName);
+    const commit = await getCommits(repos[0]);
+    console.log(commit);
+  } catch (err) {
+    console.log(err);
+  }
+}
+displayCommitsss();
 
 function displayUser(user) {
   getRepositories(user.gitHubUserName, displayRepos);
@@ -51,7 +61,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     console.log(username);
     setTimeout(() => {
-      return resolve(["rep1", "rep2", "rep3"]);
+      resolve(["rep1", "rep2", "rep3"]);
+      // reject(new Error("you a dummy"));
     }, 2000);
   });
   // console.log("my git user name is : " + username);
