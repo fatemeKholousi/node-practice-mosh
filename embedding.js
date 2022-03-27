dab04ea67c8e95de65a4457925a779d44ebf2ae8
Fateme kholousi <fatemekholousizaer@gmail.com>
2022-03-28 02:59:43 +0430

    populate

--- embedding.js
+++ embedding.js
@@ -1,34 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const authorSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
+  website: String,
 });
 
-const Author = mongoose.model('Author', authorSchema);
+const Author = mongoose.model("Author", authorSchema);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
-}));
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    authors: [authorSchema],
+  })
+);
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    authors,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function updateAuthor(courseId) {
+  //first way:
+  // const course = await Course.findById(courseId);
+  // course.author.name = "fateme kholousi";
+  // course.save();
+
+  //second way:
+  const course = await Course.update({
+    _id: courseId,
+    $set: {
+      "author.name": "john smith",
+    },
+  });
+}
+
+createCourse("Node Course", [
+  new Author({ name: "Mosh" }),
+  new Author({ name: "Fateme" }),
+]);
+// updateAuthor("6240d8bbab6ca6de38b65161");
--- population.js
+++ population.js
@@ -1,28 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-const Author = mongoose.model('Author', new mongoose.Schema({
-  name: String,
-  bio: String,
-  website: String
-}));
+const Author = mongoose.model(
+  "Author",
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String,
+  })
+);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String,
-  author:{
-   type:mongoose.Schema.Types.ObjectId,
-   ref:"Author"
-  }
-}));
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    // _id: new mongoose.Types.ObjectId(),
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
+  })
+);
 
-async function createAuthor(name, bio, website) { 
+async function createAuthor(name, bio, website) {
   const author = new Author({
-    name, 
-    bio, 
-    website 
+    name,
+    bio,
+    website,
   });
 
   const result = await author.save();
@@ -31,23 +39,27 @@ async function createAuthor(name, bio, website) {
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    author,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
-  const courses = await Course
-    .find()
-    .select('name');
+async function listCourses() {
+  const courses = await Course.find()
+    .select("name author")
+    //it's like author -->    .populate("author");
+    //it's like author.name -->    .populate("author", "name");
+    //it's like author.name without showing id -->    .populate("author", "name -_id");
+
+    .populate("author", "name -_id");
   console.log(courses);
 }
 
-// createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor("Fateme", "My exercise", "My Website");
 
-createCourse('Node Course', '623e4af3fb093dc580e915c6')
+// createCourse("My Course", "6240cd59cb9426429be80cd5");
 
-// listCourses();
+listCourses();
