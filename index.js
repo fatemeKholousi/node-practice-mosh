5c083f94eb414798acda87d7d3e7b16522518138
Fateme kholousi <fatemekholousizaer@gmail.com>
2022-04-03 02:59:53 +0430

    rental API's added

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const genres=require("./routes/genres")
-const customers=require("./routes/customers")
-const app=express()
+const rentals = require("./routes/rentals");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost/vidly-backend").then(()=>console.error("you connected to vidly backend")).catch(error=>console.log(error))
+const app = express();
 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/vidly-backend")
+  .then(() => console.error("you connected to vidly backend"))
+  .catch((error) => console.log(error));
 
-app.use(express.json())
-app.use("/api/genres",genres)
-app.use("/api/customers",customers)
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
-const port=process.env.PORT||5555;
-app.listen(port,()=>console.log(`listening from port ${port}`))
+const port = process.env.PORT || 9894;
+app.listen(port, () => console.log(`listening from port ${port}`));
--- routes/models/genre.js
+++ routes/models/genre.js
@@ -1,35 +1,18 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const genresSchema=new mongoose.Schema({
-    name:{type:String,required:true,minlength:5,maxlength:50},
-  })
-  
-  const Genre= mongoose.model("crud-genres",genresSchema)
-  // const genres = [
-  //   {
-  //     name: "slice of life",
-  //     id: 1,
-  //   },
-  //   {
-  //     name: "comedy",
-  //     id: 2,
-  //   },
-  //   {
-  //     name: "horror",
-  //     id: 3,
-  //   },
-  //   {
-  //     name: "thriler",
-  //     id: 4,
-  //   },
-  // ];
-  const validateGenres = (genre) => {
-    const schema = Joi.object({
-      name: Joi.string().min(3).required(),
-    });
-    return schema.validate(genre);
-  };
+const genresSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
+});
 
-  exports.validateGenres=validateGenres;
-  exports.Genre=Genre
+const Genre = mongoose.model("crud-genres", genresSchema);
+const validateGenres = (genre) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+};
+
+exports.genreSchema = genresSchema;
+exports.validateGenres = validateGenres;
+exports.Genre = Genre;
